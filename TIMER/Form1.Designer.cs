c9ebd6e5a861ba3f3c3281021d0c486acc18d851
seira-code <78219829+seira-code@users.noreply.github.com>
2021-01-29 22:41:46 +0300

    Add files via upload

---
+++ TIMER/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace TIMER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.mmss = new System.Windows.Forms.Label();
+            this.point = new System.Windows.Forms.Label();
+            this.Points = new System.Windows.Forms.Label();
+            this.points1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.hard = new System.Windows.Forms.RadioButton();
+            this.medium = new System.Windows.Forms.RadioButton();
+            this.easy = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(281, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mmss
+            // 
+            this.mmss.AutoSize = true;
+            this.mmss.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mmss.Location = new System.Drawing.Point(276, 288);
+            this.mmss.Name = "mmss";
+            this.mmss.Size = new System.Drawing.Size(84, 25);
+            this.mmss.TabIndex = 1;
+            this.mmss.Text = "label1";
+            // 
+            // point
+            // 
+            this.point.AutoSize = true;
+            this.point.BackColor = System.Drawing.Color.Transparent;
+            this.point.Font = new System.Drawing.Font("Verdana", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point.Location = new System.Drawing.Point(598, 174);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(85, 116);
+            this.point.TabIndex = 1;
+            this.point.Text = ".";
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // Points
+            // 
+            this.Points.AutoSize = true;
+            this.Points.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Points.Location = new System.Drawing.Point(527, 9);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(93, 25);
+            this.Points.TabIndex = 1;
+            this.Points.Text = "Points:";
+            // 
+            // points1
+            // 
+            this.points1.AutoSize = true;
+            this.points1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.points1.Location = new System.Drawing.Point(620, 9);
+            this.points1.Name = "points1";
+            this.points1.Size = new System.Drawing.Size(0, 25);
+            this.points1.TabIndex = 1;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 600;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // hard
+            // 
+            this.hard.AutoSize = true;
+            this.hard.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.hard.Location = new System.Drawing.Point(376, 304);
+            this.hard.Name = "hard";
+            this.hard.Size = new System.Drawing.Size(64, 17);
+            this.hard.TabIndex = 2;
+            this.hard.TabStop = true;
+            this.hard.Text = "Сложно";
+            this.hard.UseVisualStyleBackColor = true;
+            this.hard.CheckedChanged += new System.EventHandler(this.hard_CheckedChanged);
+            // 
+            // medium
+            // 
+            this.medium.AutoSize = true;
+            this.medium.Location = new System.Drawing.Point(376, 327);
+            this.medium.Name = "medium";
+            this.medium.Size = new System.Drawing.Size(62, 17);
+            this.medium.TabIndex = 2;
+            this.medium.TabStop = true;
+            this.medium.Text = "Средне";
+            this.medium.UseVisualStyleBackColor = true;
+            this.medium.CheckedChanged += new System.EventHandler(this.medium_CheckedChanged);
+            // 
+            // easy
+            // 
+            this.easy.AutoSize = true;
+            this.easy.Location = new System.Drawing.Point(376, 350);
+            this.easy.Name = "easy";
+            this.easy.Size = new System.Drawing.Size(56, 17);
+            this.easy.TabIndex = 2;
+            this.easy.TabStop = true;
+            this.easy.Text = "Легко";
+            this.easy.UseVisualStyleBackColor = true;
+            this.easy.CheckedChanged += new System.EventHandler(this.easy_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 389);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.easy);
+            this.Controls.Add(this.medium);
+            this.Controls.Add(this.hard);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.points1);
+            this.Controls.Add(this.Points);
+            this.Controls.Add(this.mmss);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label mmss;
+        private System.Windows.Forms.Label point;
+        private System.Windows.Forms.Label Points;
+        private System.Windows.Forms.Label points1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.RadioButton hard;
+        private System.Windows.Forms.RadioButton medium;
+        private System.Windows.Forms.RadioButton easy;
+    }
+}
+
---
+++ TIMER/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIMER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            mmss.Text = "00:00";
+            point.Visible = false;
+            point.Enabled = false;
+            easy.Checked = true;
+
+        }
+        DateTime secondmer = new DateTime();
+        int secnds;
+        int points2 = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            secnds += 1;
+            if (secnds == 1)
+            {
+                timer1.Start();
+                timer2.Start();
+                button1.Text = "Stop";
+                point.Visible = true;
+                point.Enabled = true;
+            }
+            if (secnds == 2)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                point.Enabled = false;
+                button1.Text = "Reset";
+                point.Visible = false;
+                point.Enabled = false;
+            }
+            if (secnds == 3)
+            {
+                secnds = 0;
+                points2 = 0;
+                points1.Text = "0";
+                button1.Text = "Start";
+                mmss.Text = "00:00";
+                secondmer = new DateTime(0, 0);
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            secondmer = secondmer.AddSeconds(1);
+            mmss.Text = secondmer.ToString("mm:ss");
+
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            point.Location = new Point(rand.Next(-14, 598), rand.Next(-61, 174));
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            points2 += 1;
+            points1.Text = points2 + "";
+        }
+
+        private void hard_CheckedChanged(object sender, EventArgs e)
+        {
+            timer2.Interval = 499;
+        }
+
+        private void medium_CheckedChanged(object sender, EventArgs e)
+        {
+            timer2.Interval = 650;
+        }
+
+        private void easy_CheckedChanged(object sender, EventArgs e)
+        {
+            timer2.Interval = 1000;
+        }
+    }
+}
